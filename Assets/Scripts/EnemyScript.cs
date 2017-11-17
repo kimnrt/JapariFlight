9a3a858a1c1e4465000655e66b21b16579fb5873
kimnrt <kimnrt@naver.com>
2017-11-17 17:40:13 +0900

    test
    test

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
 
+    public Transform target;
+    public float detectRange;
+
+    bool isAware = false;
+
+    int attckType;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (!isAware)
+            DetectPlayer();
+
+
+        
 	}
+
+    void DetectPlayer()
+    {
+        if (detectRange >= Vector3.Distance(target.transform.position, transform.position))
+        {
+            isAware = true;
+        }
+    }
+
+    void AttackPlayer()
+    {
+
+    }
+
+    void FireMissile()
+    {
+
+    }
+
+
 }
